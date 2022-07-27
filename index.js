26e1d6818d9dbc1cba11508d53270f4ca73ee6e0
Michael <michaelbracey24@gmail.com>
2022-07-27 12:18:50 +0100

    Added body parts and collisions with body/walls

--- index.js
+++ index.js
@@ -3,14 +3,20 @@ const ctx = canvas.getContext('2d')
 
 let tiles = 20;
 let tileSize = canvas.width / tiles -2;
+let gameOver = false
 
 const game = () => {
   resetCanvas()
   collisions()
+
+  if (gameOver) {
+    console.log("game over")
+    return
+  }
+
   drawFood()
   snakePos()
   drawSnake()
-  console.log(100)
   setTimeout(game, 200)
 }
 
@@ -23,10 +29,35 @@ let snakeX = 10;
 let snakeY = 10;
 let snakeDx = 0;
 let snakeDy = 0;
+let snakeBody = [];
+let foodEaten = 1;
+
 
 const drawSnake = () => {
+
+  // body
+  // snake part added every screen refresh where the head was at last
+  let snakePart = {
+    x: snakeX,
+    y: snakeY
+  }
+  snakeBody.push(snakePart)
+
+  // remove snake parts as it should always be equal to amount of food eaten
+  if (snakeBody.length > foodEaten) {
+    snakeBody.shift()
+  }
+
   ctx.fillStyle = "green"
+  // draw all snake body parts in array
+  for (let i = 0; i < foodEaten; i++) {
+    ctx.fillRect(snakeBody[i].x * tiles,snakeBody[i].y * tiles, tileSize, tileSize)
+  }
+
+  // draw head last
+  ctx.fillStyle = "#00ff00"
   ctx.fillRect(snakeX * tiles,snakeY * tiles, tileSize, tileSize)
+
 }
 
 const snakePos = () => {
@@ -75,10 +106,43 @@ const drawFood = () => {
 }
 
 const collisions = () => {
+
+  // food collisons
   if (snakeX === foodX && snakeY === foodY) {
     foodX = Math.floor(Math.random() * tiles)
     foodY = Math.floor(Math.random() * tiles)
+    foodEaten++
+    // console.log(snakeBody)
+  }
+
+  // body collisions
+  // last x,y pos is last head pos, so only want to see if head collides with all but last two snake parts
+  for (let i = snakeBody.length - 2; i > 0; i--) {
+    if (snakeX === snakeBody[i].x && snakeY === snakeBody[i].y) {
+      console.log("hit body")
+      console.log(snakeBody)
+      gameOver = true
+      break
+    }
+  }
+
+  // if (snakeDx === 0 && snakeDy === 0) {
+  //   gameOver = false
+  // }
+
+  // wall collisions
+  if (snakeX === tiles || snakeX < 0) {
+    gameOver = true
+  }
+
+  if (snakeY === tiles || snakeY < 0) {
+    gameOver = true
   }
 }
 
 game()
+
+// collision wall and snake body
+// game over message
+// score counter
+// add a bad apple?
