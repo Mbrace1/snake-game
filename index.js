c345d27a0dc3a79377981180b4a793491d5e1137
Michael <michaelbracey24@gmail.com>
2022-07-27 09:18:52 +0100

    Added food and tiles so collisions work

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
 const canvas = document.getElementById('canvas')
 const ctx = canvas.getContext('2d')
 
+let tiles = 20;
+let tileSize = canvas.width / tiles -2;
+
 const game = () => {
   resetCanvas()
+  collisions()
+  drawFood()
   snakePos()
   drawSnake()
-  setTimeout(game, 10)
+  console.log(100)
+  setTimeout(game, 200)
 }
 
 const resetCanvas = () => {
@@ -20,7 +26,7 @@ let snakeDy = 0;
 
 const drawSnake = () => {
   ctx.fillStyle = "green"
-  ctx.fillRect(snakeX,snakeY,20, 20)
+  ctx.fillRect(snakeX * tiles,snakeY * tiles, tileSize, tileSize)
 }
 
 const snakePos = () => {
@@ -30,28 +36,28 @@ const snakePos = () => {
 
 const snakeControls = (e) => {
   if (e.keyCode === 38) {
-    if (snakeDy === 1) {
+    if (snakeDy === 1 || snakeDy === -1) {
       return
     }
     snakeDx = 0
     snakeDy -= 1
   }
   if (e.keyCode === 40) {
-    if (snakeDy === -1) {
+    if (snakeDy === -1 || snakeDy === 1) {
       return
     }
     snakeDx = 0
     snakeDy += 1
   }
   if (e.keyCode === 39) {
-    if (snakeDx === -1) {
+    if (snakeDx === -1 || snakeDx === 1) {
       return
     }
     snakeDx += 1
     snakeDy = 0
   }
   if (e.keyCode === 37) {
-    if (snakeDx === 1) {
+    if (snakeDx === 1 || snakeDx === -1) {
       return
     }
     snakeDx -= 1
@@ -60,5 +66,19 @@ const snakeControls = (e) => {
 }
 
 document.addEventListener("keydown", snakeControls)
+
+let foodX = 5;
+let foodY = 5;
+const drawFood = () => {
+  ctx.fillStyle = "red"
+  ctx.fillRect(foodX * tiles,foodY * tiles, tileSize, tileSize)
+}
+
+const collisions = () => {
+  if (snakeX === foodX && snakeY === foodY) {
+    foodX = Math.floor(Math.random() * tiles)
+    foodY = Math.floor(Math.random() * tiles)
+  }
+}
 
 game()
